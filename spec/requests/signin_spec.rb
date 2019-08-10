7c6b067a309440309f953bd23c2a66e49d4e4410
Anton Kosolapov <anvkos@ya.ru>
2019-08-10 11:19:18 +0700

    [+] user signin

---
+++ spec/requests/signin_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /login', type: :request do
+  let(:user) { create(:user) }
+  let(:url) { '/login' }
+  let(:params) do
+    {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+  end
+
+  context 'login user' do
+    before { post url, params: params }
+
+    it 'returns 200 status code' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns a user json schema' do
+      expect(response).to match_response_schema('users/user')
+    end
+
+    it 'returns JTW token in authorization header' do
+      expect(response.headers['Authorization']).to be_present
+    end
+
+    it 'returns valid JWT token' do
+      decoded_token = decoded_jwt_token_from_response(response)
+      expect(decoded_token.first['sub']).to be_present
+    end
+  end
+
+  context 'login params are incorrect' do
+    before { post url }
+
+    it 'returns unathorized status code' do
+      expect(response.status).to eq 401
+    end
+  end
+end
