93676dda8cdd68573ab0c224ee8e7ef39602382b
Anton Kosolapov <anvkos@ya.ru>
2019-08-13 14:59:19 +0700

    [+] user can see only his accounts

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AccountsController < Api::V1::BaseController
       def index
-        @accounts = Account.all
+        @accounts = current_user.accounts
         render json: @accounts, each_serializer: AccountSerializer, adapter: :json
       end
     end
--- spec/api/v1/accounts/index_spec.rb
+++ spec/api/v1/accounts/index_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Accounts API' do
   describe "GET /api/v1/accounts" do
-    let!(:accounts) { create_list(:account, 2) }
+    let!(:user) { create(:user) }
+    let!(:accounts) { create_list(:account, 2, user: user) }
 
     context 'unauthorized' do
       it 'returns 401 status' do
@@ -17,21 +18,42 @@ RSpec.describe 'Accounts API' do
     end
 
     context 'authorized' do
-      let!(:user) { create(:user) }
       let!(:token) { auth_user(user) }
 
-      before { get_with_token('/api/v1/accounts', { format: :json }, token) }
-
       it "returns http success" do
+        get_with_token('/api/v1/accounts', { format: :json }, token)
         expect(response).to have_http_status(:success)
       end
 
       it 'returns a list of accounts' do
+        get_with_token('/api/v1/accounts', { format: :json }, token)
         expect(response.body).to have_json_size(2).at_path('accounts')
       end
 
       it 'returns accounts json schema' do
+        get_with_token('/api/v1/accounts', { format: :json }, token)
         expect(response).to match_response_schema('v1/accounts/accounts')
+      end
+
+      context 'user accounts' do
+        let!(:another_user) { create(:user) }
+        let!(:another_accounts) { create_list(:account, 2, user: another_user) }
+
+        before { get_with_token('/api/v1/accounts', { format: :json }, token) }
+
+        it 'returns user accounts' do
+          account_ids = accounts.map(&:id)
+          data = JSON.parse(response.body)
+          data_ids = data["accounts"].map { |item| item["id"] }
+          expect(data_ids).to include(*account_ids)
+        end
+
+        it 'does not return accounts to another user' do
+          another_account_ids = another_accounts.map(&:id)
+          data = JSON.parse(response.body)
+          data_ids = data["accounts"].map { |item| item["id"] }
+          expect(data_ids).to_not include(*another_account_ids)
+        end
       end
     end
   end
