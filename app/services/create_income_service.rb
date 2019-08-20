0aa41870660890c76ae8acd69e12585b14134c79
Anton Kosolapov <anvkos@ya.ru>
2019-08-20 21:27:49 +0700

    [~] create income service inherits income service

--- app/services/create_income_service.rb
+++ app/services/create_income_service.rb
@@ -1,20 +1,10 @@
-class CreateIncomeService
-  include Wisper::Publisher
-
+class CreateIncomeService < IncomeService
   def call(user, params)
     income = user.incomes.create(params)
     return publish_error(income) unless income.valid?
 
-    account = income.account
-    account.update!(amount: account.amount + income.amount)
+    account_service.increase_amount(income, income.amount)
     publish(:income_created, income)
-    income
-  end
-
-  protected
-
-  def publish_error(income)
-    publish(:income_error, income)
     income
   end
 end
